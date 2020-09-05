a9845637bc9f0d1202a650df85ead3d6e6f06296
amitdesh <amitdesh2014@gmail.com>
2020-09-05 12:56:27 -0400

    pgsearch

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,13 @@
 class BooksController < ApplicationController
+
+    def search
+        @books = Book.search_by_term(params[:query])
+    
+        render json: @books
+    
+    end
+
+
+
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,12 @@
 class Book < ApplicationRecord
+
+    include PgSearch
+    pg_search_scope :search_by_term, against [:title, :author, :genre],
+    using: {
+        tsearch:{
+            any_word: true,
+            prefix: true
+        }
+    }
+
 end
