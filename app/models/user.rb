dc58cb3f273a78891fb61bb81c54b6b8ea2431e1
amitdesh <amitdesh2014@gmail.com>
2020-09-08 21:22:53 -0400

    delete function works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :checkouts
     has_many :books, through: :checkouts
+    validates :username, presence: true
+    validates :username, uniqueness: true
 end
